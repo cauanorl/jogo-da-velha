22e5e82f9e7a0dbf9e31d0f30410beebd7ea1cca
Cauanorl <rodriguescauan701@gmail.com>
2021-12-15 19:38:13 -0300

    jogo finalizado

--- js/main.js
+++ js/main.js
@@ -7,7 +7,30 @@ game = {
          this.$secondLine,
          this.$thirdLine
       ])
+      this.gameButtonsSelector()
+      this.bindEvents()
 
+   },
+
+   bindEvents: function() {
+      for (let div of this.$divs) {
+         div.addEventListener('click', event => {
+            if (!this.finish) {
+
+               if (event.target.outerText === '') {
+                  const index = parseInt(event.target.dataset.value)
+                  
+                  this.changeActual()
+                  event.target.innerText = this.actual
+                  
+                  this.listGame[index - 1] = this.actual
+                  if (this.victoryCondition()) {
+                     this.gameFinish()
+                  }
+               }
+            }
+         })
+      }
    },
 
    casheSelectors: function() {
@@ -17,20 +40,71 @@ game = {
    },
 
    gameButtonsSelector: function() {
-      this.$div1 = document.querySelectorAll('#divGame')
-
+      this.$divs = document.querySelectorAll('#divGame')
+      this.listGame = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+      this.actual = 'O'
+      this.finish = false
    },
 
    createDiv: function(values) {
+      let count = 0
       for (let value of values) {
          value.innerHTML += `
-            <div id='divGame' class='game'></div>
-            <div  id='divGame' class='game'></div>
-            <div id='divGame' class='game'></div>
+            <div id='divGame' class='game' data-value="${count+1}"></div>
+            <div id='divGame' class='game' data-value="${count+2}"></div>
+            <div id='divGame' class='game' data-value="${count+3}"></div>
          `
+         count += 3
       }
+   },
+
+   changeActual: function() {
+      if (this.actual === 'O') {
+         this.actual = 'X'
+         return
+      }
+      else {
+         this.actual = 'O'
+         return
+      }
+   },
+
+   victoryCondition: function() {
+      /*
+      [0] [3] [6]
+      [1] [4] [7]
+      [2] [5] [8]
+      */
+
+      const win = this.listGame
+      let end = false
+      
+      for (let count = 0; count < 9; count += 3) {
+         if (win[count] === win[count+1] && win[count+1] === win[count+2]) {
+            end = true
+        }  
+      }
+      for (let count = 0;count < 3;count++) {
+         if (win[count] === win[count+3] && win[count+3] === win[count+6]) {
+            end = true
+         }
+      }
+      if (win[0] === win[4] && win[4] === win[8]) {
+         end = true
+      }
+      else if (win[2] === win[4] && win[4] === win[6]) {
+         end = true
+      }
+
+      if (end) {
+         
+         return true
+      }
+   },
+
+   gameFinish: function() {
+      this.finish = true
    }
 }
-
 
 game.init()
